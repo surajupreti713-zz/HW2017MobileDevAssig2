25e3bb3fe10832312bad518a0cfbc773d5d1cd39
Suraj Upreti <supreti@supreti-macbookpro.roam.corp.google.com>
2017-08-01 22:36:40 -0700

    added music playing functionality

--- app/src/main/java/com/suraj/itunessearch/MusicListFragment.java
+++ app/src/main/java/com/suraj/itunessearch/MusicListFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,16 +23,22 @@ import com.android.volley.toolbox.NetworkImageView;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.suraj.itunessearch.R.id.parent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 
 public class MusicListFragment extends Fragment {
 
     private ListView mListView;
 
     private MusicAdapter mMusicAdapter;
+    private MediaPlayer mMediaPlayer;
+    private String mCurrentlyPlayingUrl;
+
 
 
     private SwipeRefreshLayout mSwipeRefreshLayout;
@@ -51,7 +58,10 @@ public class MusicListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
         searchText = getArguments().getString("data");
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
     }
 
     @Override
@@ -102,6 +112,39 @@ public class MusicListFragment extends Fragment {
 
         return v;
     }
+
+    private void clickedAudioURL(String url) {
+        if (mMediaPlayer.isPlaying()) {
+            if (mCurrentlyPlayingUrl.equals(url)) {
+                mMediaPlayer.stop();
+                mMusicAdapter.notifyDataSetChanged();
+                return;
+            }
+        }
+
+        mCurrentlyPlayingUrl = url;
+        try {
+            mMediaPlayer.reset();
+            mMediaPlayer.setDataSource(url);
+            mMediaPlayer.prepareAsync(); // might take long! (for buffering, etc)
+            mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                    mMusicAdapter.notifyDataSetChanged();
+                }
+            });
+            mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mMusicAdapter.notifyDataSetChanged();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private void refreshMusic() {
         ItunesMusicSource.get(getContext()).getMusicItems(new ItunesMusicSource.MusicListener() {
@@ -164,6 +207,26 @@ public class MusicListFragment extends Fragment {
             mImageView = (NetworkImageView) rowView.findViewById(R.id.networkImageView);
             ImageLoader loader = ItunesMusicSource.get(getContext()).getImageLoader();
             mImageView.setImageUrl(music.getmArtworkUrl60(), loader);
+
+
+            final ImageButton playButton = (ImageButton) rowView.findViewById(R.id.play_button);
+            boolean isPlaying = mMediaPlayer.isPlaying() && mCurrentlyPlayingUrl.equals(music.getmPreviewUrl());
+
+            if (isPlaying) {
+                playButton.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_media_pause));
+            }
+            else {
+                playButton.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_media_play));
+            }
+
+            playButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("aas", "it woooooorkssssss!!!!!");
+                    clickedAudioURL(music.getmPreviewUrl());
+                }
+            });
+
 
             return rowView;
         }
